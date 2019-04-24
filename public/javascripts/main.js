b45b604e919e4e8e1ab1110fae34402ddf379e9a
x00145570 <x00145570@outlook.com>
2019-04-24 02:37:51 +0100

    backtotop and show password feature added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -40,3 +40,46 @@ $('body').append(log);
     var me = $(this);
     log.html(me.attr('value'));
   });
+
+
+  //showpassword
+  function myFunction() {
+    var x = document.getElementById("pass");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  };
+  function myFunction2() {
+    var x = document.getElementById("pass2");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  };
+  function myFunction3() {
+    var x = document.getElementById("pass3");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  };
+
+  // Backtotop
+     
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+       document.getElementById("top").style.display = "block";
+   } else {
+       document.getElementById("top").style.display = "none";
+   }
+}
+function topFunction() {
+   document.body.scrollTop = 0; 
+   document.documentElement.scrollTop = 0; 
+}
